e3a117defce98b09143da74f5522c60f4ef2c6dd
Sarah-zzzine <intissarezzidiv@gmail.com>
2023-10-16 22:24:10 +0100

    Group Projects

---
+++ handle_print.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * handle_print -A funrction that Prints an argument based on its type
+ * @format: The formatted Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ */
+int handle_print(const char *format, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+	format_t format_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; format_types[i].format != '\0'; i++)
+		if (format[*ind] == format_types[i].format)
+			return (format_types[i].fn(list, buffer, flags, width, precision, size));
+
+	if (format_types[i].format == '\0')
+	{
+		if (format[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (format[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (format[*ind] != ' ' && format[*ind] != '%')
+				--(*ind);
+			if (format[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
+
